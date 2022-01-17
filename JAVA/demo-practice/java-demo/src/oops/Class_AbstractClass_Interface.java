f98be2a69ef1556f3687d972639c79d1e088e53a
DEV <dev@dev.com>
2022-01-17 10:32:50 +0530

    commit..

--- JAVA/demo-practice/java-demo/src/oops/Class_AbstractClass_Interface.java
+++ JAVA/demo-practice/java-demo/src/oops/Class_AbstractClass_Interface.java
@@ -2,10 +2,21 @@ package oops;
 
 public class Class_AbstractClass_Interface {
 	public static void main(String[] args) {
-		Account account;
+//		Account account;
 //		Cannot instantiate the type Account
 //		account = new Account();
+//		NetBanking netBanking;
+//		Cannot instantiate the type NetBanking
+//		netBanking = new NetBanking();
 	}
+}
+//interfaces have abstract methods
+interface NetBanking {
+	public abstract void transferAmount();
+}
+//interfaces have abstract methods
+interface DebitCard {
+	public abstract void withdrawAmount();
 }
 
 // abstract class cannot be instantiated
@@ -20,11 +31,29 @@ abstract class Account {
 		System.out.println("CITI Bank");
 	}
 }
-class SavingsAccount extends Account{
-
+// A class can extend from only one class but can implement multiple interfaces.
+class SavingsAccount extends Account implements NetBanking, DebitCard{
+// override the abstract method
 	@Override
 	public void displayAccountInfo() {
-		System.out.println("Savings Account");
+		System.out.println("Savings Account Info");
+	}
+
+	@Override
+	public void transferAmount() {
+		System.out.println("Transfer amount from Savings Bank Account using NetBanking");
+	}
+
+	@Override
+	public void withdrawAmount() {
+		System.out.println("Withdraw amount from Savings Bank Account using DebitCard");
+		
 	}	
 }
-// Create Current account and display "Current Account"
+class CurrentAccount extends Account{
+	// override the abstract method
+		@Override
+		public void displayAccountInfo() {
+			System.out.println("Current Account Info");
+		}	
+	}
