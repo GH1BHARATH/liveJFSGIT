4af5ee3adbbe97a182fbf52dab007bbf65e7203a
DEV <dev@dev.com>
2022-01-17 09:55:46 +0530

    commit..

---
+++ JAVA/demo-practice/java-demo/src/oops/Class_AbstractClass_Interface.java
@@ -0,0 +1,19 @@
+package oops;
+
+public class Class_AbstractClass_Interface {
+	public static void main(String[] args) {
+		Account account;
+//		Cannot instantiate the type Account
+//		account = new Account();
+	}
+}
+
+// abstract class cannot be instantiated
+// inheritance is the purpose of abstract classes
+abstract class Account {
+//	Abstract methods do not specify a body
+//	public abstract void displayAccountInfo() {
+//		
+//	}
+	public abstract void displayAccountInfo();
+}
