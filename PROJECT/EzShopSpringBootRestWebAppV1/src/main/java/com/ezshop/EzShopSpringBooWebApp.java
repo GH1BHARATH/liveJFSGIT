c30a13254b8f790febaf6c4d716c359b0897b071
DEV <dev@dev.com>
2022-02-04 19:43:35 +0530

    commit..

--- PROJECT/EzShopSpringBootRestWebAppV1/src/main/java/com/ezshop/EzShopSpringBooWebApp.java
+++ PROJECT/EzShopSpringBootRestWebAppV1/src/main/java/com/ezshop/EzShopSpringBooWebApp.java
@@ -1,13 +1,13 @@
-package com.ezshop;
-
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
-public class EzShopSpringBooWebApp {
-
-	public static void main(String[] args) {
-		SpringApplication.run(EzShopSpringBooWebApp.class, args);
-	}
-
-}
+//package com.ezshop;
+//
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+//
+//@SpringBootApplication
+//public class EzShopSpringBooWebApp {
+//
+//	public static void main(String[] args) {
+//		SpringApplication.run(EzShopSpringBooWebApp.class, args);
+//	}
+//
+//}
