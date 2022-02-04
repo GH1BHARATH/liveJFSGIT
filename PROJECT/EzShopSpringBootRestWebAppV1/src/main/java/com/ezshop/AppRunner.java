b11e8777916f16abde5567ea19bf67d4a4968200
DEV <dev@dev.com>
2022-02-04 17:45:53 +0530

    commit..

---
+++ PROJECT/EzShopSpringBootRestWebAppV1/src/main/java/com/ezshop/AppRunner.java
@@ -0,0 +1,11 @@
+package com.ezshop;
+
+import org.springframework.boot.CommandLineRunner;
+
+public class AppRunner implements CommandLineRunner{
+	@Override
+	public void run(String... args) throws Exception {
+		
+	}
+	
+}
--- PROJECT/EzShopSpringBootRestWebAppV1/src/main/java/com/ezshop/repository/ProductRepository.java
+++ PROJECT/EzShopSpringBootRestWebAppV1/src/main/java/com/ezshop/repository/ProductRepository.java
@@ -1,9 +1,10 @@
 package com.ezshop.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.ezshop.entity.Product;
-
+@Repository
 public interface ProductRepository extends JpaRepository<Product, String>{
 	
 }
